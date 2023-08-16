dc9066fd7b55d77a50a47d88cbdfd65b4792e930
zafajardo9 <zafajardo9.programming@gmail.com>
2023-08-16 23:05:55 +0800

    establishing connection to postgres

--- universityresearch/settings.py
+++ universityresearch/settings.py
@@ -67,8 +67,10 @@ WSGI_APPLICATION = 'universityresearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
