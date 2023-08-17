59810503decef613b4a7816b7ef2fe54e3baeddf
zafajardo9 <zafajardo9.programming@gmail.com>
2023-08-18 06:32:37 +0800

    ewan

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,15 @@
+const menuButton = document.getElementById("menu-button");
+const menuDiv = document.getElementById("menu-div");
+
+
+menuButton.addEventListener("click", () => {
+    menuDiv.classList.toggle("hidden");
+});
+
+
+
+// EWAN
+
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
 
